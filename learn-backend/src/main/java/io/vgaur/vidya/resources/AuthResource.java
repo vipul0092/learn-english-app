c6c58422d2db4cfd616ebad089f6f93a447a8828
Vipul Gaur <sunny3091@gmail.com>
2020-07-27 19:46:08 +0530

    feat: Return some additional data from auth endpoint for verification

--- learn-backend/src/main/java/io/vgaur/vidya/resources/AuthResource.java
+++ learn-backend/src/main/java/io/vgaur/vidya/resources/AuthResource.java
@@ -5,6 +5,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by vgaur created on 27/07/20
@@ -16,6 +18,9 @@ public class AuthResource {
 
     @GET
     public Response getAuthResponse() {
-        return Response.ok().build();
+        Map<String, String> value = new HashMap<>();
+        value.put("name", "VGaur");
+        value.put("location", "AWS EC2");
+        return Response.ok(value).build();
     }
 }
